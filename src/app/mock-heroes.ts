3a9dc7e978fd0d301609a175126ee196504c0e00
bqanderson <bqanderson@me.com>
2018-06-05 22:52:12 -0500

    Merge conflict.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Willhelm' },
+  { id: 12, name: 'Brother Tiberius' },
+  { id: 13, name: 'Jonah' },
+  { id: 14, name: 'Scarlet' },
+  { id: 15, name: 'Yoshime' },
+  { id: 16, name: 'Red Sonja' },
+  { id: 17, name: 'Faolen' },
+  { id: 18, name: 'Ra\'ag' },
+  { id: 19, name: 'Cutty LeSark' },
+  { id: 20, name: 'Tree, The Oak King' }
+];
